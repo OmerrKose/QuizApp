7f1422bdd43fc674f24eff0794b1c1225e5d8e52
OmerrKose <omer.kose@ibtech.com.tr>
2021-04-30 15:31:45 +0300

    Added progress bar

--- app/src/main/java/com/quizapp/Questions.kt
+++ app/src/main/java/com/quizapp/Questions.kt
@@ -1,4 +1,12 @@
 package com.quizapp
 
-class Questions {
-}
+data class Questions(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
