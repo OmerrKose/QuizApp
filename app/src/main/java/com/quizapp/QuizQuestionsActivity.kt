f7327b0ccf595f2d27c89c406f8e203eb2e57e88
OmerrKose <omer.kose@ibtech.com.tr>
2021-04-30 17:09:37 +0300

    Added choice page for the questions

--- app/src/main/java/com/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/quizapp/QuizQuestionsActivity.kt
@@ -2,12 +2,24 @@ package com.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionList = Constants.getQuestions()
+        val currentPosition = 1
+        val questions: Questions = Constants.getQuestions()[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tvQuestionId.text = questions!!.question
+        ivImage.setImageResource(questions.image)
+        tvOptionOne.text = questions.optionOne
+        tvOptionTwo.text = questions.optionTwo
+        tvOptionThree.text = questions.optionThree
+        tvOptionFour.text = questions.optionFour
     }
 }
